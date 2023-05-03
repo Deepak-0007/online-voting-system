41c5e160b093dcbee8ecd5f2a0d69f2d6f467013
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-05-03 23:15:05 +0530

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,8 @@
+public class reversestring {
+    public static void main(String[] args) {
+        StringBuffer s=new StringBuffer("this is reverse function");
+        s.reverse();
+        System.out.println("after the reverse of string "+s);
+    }
+    
+}
