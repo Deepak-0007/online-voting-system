57f348b1a31aa71fdc44a16ddd4be4002cfa47f8
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-06-04 20:56:57 +0530

    Delete reversestring.java

--- reversestring.java
+++
@@ -1,8 +0,0 @@
-public class reversestring {
-    public static void main(String[] args) {
-        StringBuffer s=new StringBuffer("this is reverse function");
-        s.reverse();
-        System.out.println("after the reverse of string "+s);
-    }
-    
-}
